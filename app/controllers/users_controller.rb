5890df1cb4ec194c1e09893605d13727f0d664fe
leesc22 <leesiewching22@gmail.com>
2017-09-15 12:09:55 +0800

    Install Clearance gem for user management

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < Clearance::UsersController
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+  private
+
+  def user_from_params
+  	name = user_params.delete(:name)
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.name = name
+      user.email = email
+      user.password = password
+    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
+  end
+
+end
