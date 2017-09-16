fdef6703310c2630c6365874b17c7bb76d7d193a
leesc22 <leesiewching22@gmail.com>
2017-09-16 16:56:56 +0800

    Install Bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   # Redirect user from Facebook login
   get '/auth/:provider/callback' => 'sessions#create_from_omniauth'
   
-  resources :listings
+  resources :listings, except: [:index]
   get "/users/:id/listings" => "listings#user_listings", as: "user_listings"
 
   # Set up root
