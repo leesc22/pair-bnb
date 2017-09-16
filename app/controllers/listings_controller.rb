07bd039891a4f1ac4d967826474f676cb5b072f8
leesc22 <leesiewching22@gmail.com>
2017-09-16 12:59:10 +0800

    Set up new and create for listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,8 +12,8 @@ class ListingsController < ApplicationController
 	end
 
 	def create
-		@listing = Listing.new(listing_params)
-		if @listng.save
+		@listing = current_user.listings.new(listing_params)
+		if @listing.save
 			redirect_to @listing
 		else
 			render 'new'
