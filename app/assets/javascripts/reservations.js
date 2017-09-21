d020b2555682f8e952c0917c23413f58c60ab222
leesc22 <leesiewching22@gmail.com>
2017-09-21 11:02:35 +0800

    Set checkout date to one day after checkin date - UX

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
 		format: 'DD-MM-YYYY',
 		minDate: new Date(),
 	});
-
-	// $checkin_date_string = $('.datetimepicker_in')[0].value;
-	// $checkin_date_string_split = $checkin_date_string.split('-');
-	// $checkin_date = new Date($checkin_date_string_split[2], $checkin_date_string_split[1] - 1, $checkin_date_string_split[0])
+	$('.datetimepicker_in').on('dp.change', function(e) {	
+		$checkin_date_string = e.target.value;
+		$checkin_date_string_split = $checkin_date_string.split('-');
+		$checkin_date = new Date($checkin_date_string_split[2], $checkin_date_string_split[1] - 1, $checkin_date_string_split[0])
+		$checkin_date.setDate($checkin_date.getDate() + 1)
+		$('.datetimepicker_out').data("DateTimePicker").minDate($checkin_date)
+	});
 	
 	$('.datetimepicker_out').datetimepicker({
 		format: 'DD-MM-YYYY',
-		// minDate: new Date($checkin_date),
 		minDate: new Date(),
 	});
 })
