9b21e121b4d05a002c6f0210d2da2c775c451903
leesc22 <leesiewching22@gmail.com>
2017-09-25 19:05:31 +0800

    add default url options for action mailer link_to

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'pairbnb-leesc.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
