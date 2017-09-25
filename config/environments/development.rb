602c1e480f37c04a630f7188834991cf7d543938
leesc22 <leesiewching22@gmail.com>
2017-09-25 11:46:42 +0800

    Configure to use gmail to send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,18 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  # config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => 'gmail.com',
+   :user_name            => ENV['GMAIL_USERNAME'],
+   :password             => ENV['GMAIL_PASSWORD'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true 
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
