e72317ae69744d15d49b362a398385710199ebe5
leesc22 <leesiewching22@gmail.com>
2017-10-02 16:47:24 +0800

    Write new reservation feature spec

--- spec/features/new_reservations_spec.rb
+++ spec/features/new_reservations_spec.rb
@@ -15,10 +15,9 @@ RSpec.feature "NewReservations", type: :feature do
   	click_button 'Book'
   	expect(page.current_path).to eq reservation_path(listing.reservations.last)
   	click_link 'Make Payment'
-  	# expect(page.current_path).to eq "/braintree/new"
   	expect(page.current_url).to eq "http://www.example.com/braintree/new?reservation_id=#{listing.reservations.last.id}"
-		fill_in('credit-card-number', :with => '4111 1111 1111 1111')
-		fill_in('expiration', :with => '10/2019')
-		fill_in('cvv', :with => '123')  	
+		# fill_in('credit-card-number', :with => '4111 1111 1111 1111')
+		# fill_in('expiration', :with => '10/2019')
+		# fill_in('cvv', :with => '123')  	
   end
 end
